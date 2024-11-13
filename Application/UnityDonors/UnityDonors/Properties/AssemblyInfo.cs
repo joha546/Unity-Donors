26d9161824ea192b50c17b2404eb4ee619279b30
Md Khaled Bin Joha <joh60483@gmail.com>
2024-11-13 23:04:46 +0600

    Home Route & View

--- Application/UnityDonors/UnityDonors/Properties/AssemblyInfo.cs
+++ Application/UnityDonors/UnityDonors/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UnityDonors")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Connect People")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Error Brothers 2.0")]
 [assembly: AssemblyProduct("UnityDonors")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
