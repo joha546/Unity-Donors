0d92e40cd369919c02456bfd6804e6526003e471
Md Khaled Bin Joha <joh60483@gmail.com>
2025-01-07 16:18:50 +0600

    Select User Type Registration View

--- Application/UnityDonors/UnityDonors/Controllers/RegistrationController.cs
+++ Application/UnityDonors/UnityDonors/Controllers/RegistrationController.cs
@@ -9,10 +9,77 @@ namespace UnityDonors.Controllers
 {
     public class RegistrationController : Controller
     {
-        public ActionResult UserRegistration()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SelectUser(RegisterationMV registerationMV)
         {
+            if (registerationMV.UserTypeID == 2)
+            {
+                return RedirectToAction("DonorUser");
+            }
+            else if(registerationMV.UserTypeID == 6)
+            {
+                return RedirectToAction("SeekerUser");
+            }
+            else if (registerationMV.UserTypeID == 1 || registerationMV.UserTypeID == 4)
+            {
+                return RedirectToAction("HospitalUser");
+            }
+
+            else if (registerationMV.UserTypeID == 5)
+            {
+                return RedirectToAction("BloodBankUser");
+            }
+            else
+            {
+                return RedirectToAction("MainHome", "Home");
+            }
             var registration = new RegisterationMV();
             return View(registration);
+        }
+
+        public ActionResult HospitalUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HospitalUser(HospitalMV hospitalMV)
+        {
+            return View();
+        }
+
+        public ActionResult DonorUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DonorUser(DonorMV donorMV)
+        {
+            return View();
+        }
+
+        public ActionResult BloodBankUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult BloodBankUser(BloodBankMV bloodbankMV)
+        {
+            return View();
+        }
+
+        public ActionResult SeekerUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SeekerUser(SeekerMV seekerMV)
+        {
+            return View();
         }
     }
 }
