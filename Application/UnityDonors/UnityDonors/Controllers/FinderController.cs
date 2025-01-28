57961606aaa7089620537e7a4be7b1455e1284a4
Md Khaled Bin Joha <joh60483@gmail.com>
2025-01-28 22:56:24 +0600

    Seeker Blood Request

--- Application/UnityDonors/UnityDonors/Controllers/FinderController.cs
+++ Application/UnityDonors/UnityDonors/Controllers/FinderController.cs
@@ -74,5 +74,145 @@ namespace UnityDonors.Controllers
             ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", finderMV.CityID);
             return View(finderMV);
         }
+
+        public ActionResult RequestForBlood(int? donorid, int? usertypeid, int? bloodgroupid)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return Redirect("/Home/MainHome#registerationsection");
+            }
+
+            var request = new RequestMV();
+            request.AcceptedID = (int)donorid;
+            
+            if (usertypeid == 2)
+            {
+                request.AcceptedTypeID = 1;
+            }
+            else if (usertypeid == 6)
+            {
+                request.AcceptedTypeID = 2;
+            }
+
+            request.RequiredBloodGroupID = (int)bloodgroupid;
+
+            return View(request);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RequestForBlood(RequestMV requestMV)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return Redirect("/Home/MainHome#registerationsection");
+            }
+
+            int UserTypeID = 0;
+            int RequestTypeID = 0;
+            int RequestByID = 0;
+            int.TryParse(Convert.ToString(Session["UserTypeID"]), out UserTypeID);
+
+            if (UserTypeID == 2) // Donor Sessions
+            {
+                int.TryParse(Convert.ToString(Session["DonorID"]), out RequestByID);
+            }
+            else if (UserTypeID == 4) // Seeker Sessions
+            {
+                RequestTypeID = 1;
+                int.TryParse(Convert.ToString(Session["SeekerID"]), out RequestByID);
+            }
+            else if (UserTypeID == 5) // Hospital Sessions
+            {
+                RequestTypeID = 2;
+                int.TryParse(Convert.ToString(Session["HospitalID"]), out RequestByID);
+            }
+            else if (UserTypeID == 6) // BloodBank Sessions
+            {
+                RequestTypeID = 3;
+                int.TryParse(Convert.ToString(Session["BloodBankID"]), out RequestByID);
+            }
+
+            if (ModelState.IsValid)
+            {
+                var request = new RequestTable();
+                
+                request.RequestDate = DateTime.Now;
+                request.AcceptedTypeID = requestMV.AcceptedTypeID;
+                request.AcceptedID = requestMV.AcceptedID;
+                request.RequiredBloodGroupID = requestMV.RequiredBloodGroupID;
+                request.ExpectedDate = requestMV.ExpectedDate;
+                request.RequestDetails = requestMV.RequestDetails;
+                request.RequestStatusID = 1;
+                request.RequestByID = RequestByID;
+                request.RequestTypeID = RequestTypeID;
+
+                DB.RequestTables.Add(request);
+                DB.SaveChanges();  
+                return RedirectToAction("ShowAllResults");
+
+            }
+            return View(requestMV);
+        }
+
+        public ActionResult ShowAllResults()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            int UserTypeID = 0;
+            int RequestTypeID = 0;
+            int RequestByID = 0;
+            int.TryParse(Convert.ToString(Session["UserTypeID"]), out UserTypeID);
+
+            if (UserTypeID == 2) // Donor Sessions
+            {
+                int.TryParse(Convert.ToString(Session["DonorID"]), out RequestByID);
+            }
+            else if (UserTypeID == 4) // Seeker Sessions
+            {
+                RequestTypeID = 1;
+                int.TryParse(Convert.ToString(Session["SeekerID"]), out RequestByID);
+            }
+            else if (UserTypeID == 5) // Hospital Sessions
+            {
+                RequestTypeID = 2;
+                int.TryParse(Convert.ToString(Session["HospitalID"]), out RequestByID);
+            }
+            else if (UserTypeID == 6) // BloodBank Sessions
+            {
+                RequestTypeID = 3;
+                int.TryParse(Convert.ToString(Session["BloodBankID"]), out RequestByID);
+            }
+
+            var requests = DB.RequestTables.Where(r => r.RequestByID == RequestByID && r.RequestTypeID == RequestTypeID).ToList();
+            //var list = new List<RequestMV>();
+
+            //foreach(var request in requests)
+            //{
+            //    var addrequest = new RequestMV();
+
+            //    addrequest.RequestID = request.RequestID;
+            //    addrequest.RequestDate = request.RequestDate;
+            //    addrequest.RequestByID = request.RequestID;
+            //    addrequest.AcceptedID = request.AcceptedID;
+            //    addrequest.AcceptedFullName = "";
+            //    addrequest.AcceptedTypeID = request.RequestID;
+            //    addrequest.AcceptedType = "";
+            //    addrequest.RequiredBloodGroupID = request.RequiredBloodGroupID;
+            //    addrequest.BloodGroup = "";
+            //    addrequest.RequestTypeID = request.RequestID;
+            //    addrequest.RequestType = "";
+            //    addrequest.RequestStatus = "";
+            //    addrequest.RequestStatusID = request.RequestStatusID;
+            //    addrequest.ExpectedDate = request.ExpectedDate;
+            //    addrequest.RequestDetails = request.RequestDetails;
+
+            //    list.Add(addrequest);
+            //}
+            return View(requests);
+        }
     }
 }
